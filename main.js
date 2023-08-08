9ba0d87af4a398f1feab34571f0b2db26809a812
Attila István Kiss <kissati23@gmail.com>
2023-08-08 16:18:11 +0200

    Update main.js

--- main.js
+++ main.js
@@ -85,3 +85,30 @@ ScrollReveal().reveal(
 );
 
 /* EMAIL JS */
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_8ujqyfe','template_ttu95l1','#contact-form','-vMhhdP7IZv_7JpT3')
+    .then(() =>{
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+
+    }, () =>{
+        // Show error message
+        contactMessage.textContent = 'Message not sent (service error) ❌'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
